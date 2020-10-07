e48d8b09b11b9bbfcb4eed7225d133642510358f
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-10-07 03:26:18 +0000

    installed request npm module and did some testings getting data form our weather API using http requests

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-console.log('Starting')
-
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
-
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
+const request = require('request');
 
 
-console.log('Stopping')
+const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=b07e17544ba318a54b709a0b75ff26ca'
+
+request({ url: url }, (error, response) => {
+   // console.log(response); we get everything printed
+
+   const data = JSON.parse(response.body);
+
+   //console.log(data) we get all the body data
+
+   console.log(data.current);  // we get the current information 
+
+});
