ca2c3fe951b140291c22c935f92326ea78452967
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-10-07 16:14:27 +0000

    costumized my http requests

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const request = require('request');
 
 
-const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=b07e17544ba318a54b709a0b75ff26ca'
+const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=b07e17544ba318a54b709a0b75ff26ca&units=metric'
 
-request({ url: url }, (error, response) => {
+request({ url: url, json: true }, (error, response) => {    // by adding json: true, the response will be already an object. we dont need to parse it
    // console.log(response); we get everything printed
 
-   const data = JSON.parse(response.body);
+   //const data = JSON.parse(response.body); // without json: true
 
    //console.log(data) we get all the body data
 
-   console.log(data.current);  // we get the current information 
+   //console.log(data.current);  // we get the current information 
+
+   console.log(response.body.current)
+
+   console.log(response.body.current.weather[0].description + '. It is currently ' + response.body.current.temp + ' degrees out! There is a ' + response.body.current.humidity +
+    ' % of humidity')
+
+
 
 });
