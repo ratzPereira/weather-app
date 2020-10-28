15f1da81683b92a313a89f2df9544e4f871c9ef7
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-10-28 00:50:38 -0100

    made new file for geocode function. its more clean now

--- app.js
+++ app.js
@@ -25,29 +25,7 @@ const request = require('request');
 // });
 
 
-
-//   >>MapBox API<<
-
-// const locationURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmF0eiIsImEiOiJja2Z6bTluaHAwNGllMnpxcTljeDgxMWljIn0.XMlKn48-c8EkHMF7vQNB9w'
-
-// request({url: locationURL, json: true}, (error,response) =>{
-
-//    if (error){
-//       console.log('Unable to connect to weather service!')
-//    } else if (response.body.message === 'Not Found') {
-//       console.log('Unable to find location')
-//    } else {
-//       const latitude = response.body.features[0].center[1];
-//       const longitude = response.body.features[0].center[0];
-//       const place = response.body.features[0].place_name;
-
-//       console.log('Current location is: ' + place + ' and the coordinates are: ' + latitude + ' and ' + longitude);
-//    }
-   
-
-// });
-
-
+//Mapbox API
 
 const geocode = (address, callback) => {
    const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoicmF0eiIsImEiOiJja2Z6bTluaHAwNGllMnpxcTljeDgxMWljIn0.XMlKn48-c8EkHMF7vQNB9w'
@@ -65,8 +43,6 @@ const geocode = (address, callback) => {
          })
       }
    })
-
-
 }
 
 geocode('las vegas', (error, data) => {
---
+++ utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const geocode = require('/utils/geocode')
+
+//Mapbox API
+
+const geocode = (address, callback) => {
+    const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoicmF0eiIsImEiOiJja2Z6bTluaHAwNGllMnpxcTljeDgxMWljIn0.XMlKn48-c8EkHMF7vQNB9w'
+ 
+    request ({ url: url, json: true}, (error,response) => {
+       if (error){
+          callback('Unable to connect to weather service!', undefined) // we dont need to pass undefined here as the second arg
+       } else if (response.body.features.length === 0) {
+          callback('Unable to find location', undefined) // we dont need to pass undefined here as the second arg
+       } else {
+          callback(undefined, {
+             latitude: response.body.features[0].center[0],
+             longitude: response.body.features[0].center[1],
+             location: response.body.features[0].place_name
+          })
+       }
+    })
+}
+
+module.exports = geocode
