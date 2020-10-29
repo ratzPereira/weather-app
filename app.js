c6261ce941603008f461fa03e55cb868e08b35c3
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-10-29 15:45:49 -0100

    fixed one bug with latitude and longitude

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const request = require('request');
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 
 
 //Mapbox API
-geocode('las vegas', (error, data) => {
+geocode('Las Vegas', (error, data) => {
    console.log('Error', error)
    console.log('Data', data)
-})
-
-
-//Forecast
-forecast(-75.7088, 44.1545, (error, data) => {
-   console.log('Error', error)
-   console.log('Data', data)
+   forecast(data.latitude, data.longitude, (error, data) => {
+      console.log('Error', error)
+      console.log('Data', data)
+   })
 })
--- utils/geocode.js
+++ utils/geocode.js
@@ -12,8 +12,8 @@ const geocode = (address, callback) => {
           callback('Unable to find location', undefined) // we dont need to pass undefined here as the second arg
        } else {
           callback(undefined, {
-             latitude: response.body.features[0].center[0],
-             longitude: response.body.features[0].center[1],
+             latitude: response.body.features[0].center[1],
+             longitude: response.body.features[0].center[0],
              location: response.body.features[0].place_name
           })
        }
