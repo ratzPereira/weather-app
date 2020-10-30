f20cd25cc3cc0700ee74fadc89f033ed46456313
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-10-30 18:23:24 -0100

    Did some destructuring and property shorthand changes!

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ if (address.length === 0) {
    return console.log('Please insert one valid location name')
 } else {
 
-   geocode(city, (error, data) => {
+   geocode(address, (error, { latitude, longitude, location} = {}) => {
    if(error){
        return console.log(error)  //if we had an error the function stops here because we have "return"
    }
 
-      forecast(data.latitude, data.longitude, (error, forecastData) => {
+      forecast(latitude, longitude, (error, forecastData) => {
          if(error){
             return console.log(error)  //if we had an error the function stops here because we have "return"
          }
 
-         console.log(data.location)
+         console.log(location)
          console.log(forecastData)
       })
    })
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + encodeURIComponent(latitude) + '&lon=' + encodeURIComponent(longitude) + '&appid=b07e17544ba318a54b709a0b75ff26ca&units=metric'
 
-    request ({url: url, json: true}, (error, response) => {
+    request ({url, json: true}, (error, {body}) => {
         if(error){
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.message === 'Nothing to geocode') {
+        } else if (body.message === 'Nothing to geocode') {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather[0].description + '. It is currently ' + response.body.current.temp + ' degrees out! There is a ' + response.body.current.humidity + '% of humidity')
+            callback(undefined, body.current.weather[0].description + '. It is currently ' + body.current.temp + ' degrees out! There is a ' + body.current.humidity + '% of humidity')
         }
     })
 }
